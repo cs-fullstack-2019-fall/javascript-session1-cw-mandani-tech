26752e436b548b51b687bcf40753583e1f47b13a
mandani-tech <mandani.hamida@gmail.com>
2019-08-19 16:35:27 -0500

    cw with challenge complete

---
+++ index.js
@@ -0,0 +1,90 @@
+// # javascript-session1-cw
+//
+// Try to do this without using the code from previous work.
+// Only use the JavaScript reference guide: https://docs.google.com/document/d/1sdPHe4xQt_qEzbePtZRdTOcX2hDWpBVV8E_L8JvcPrI/
+//
+//     Create a FacebookUser class with one attribute: name.
+//     In a main function use the array below to create an array of
+//     FacebookUser objects and console.log that new array.
+//
+//     ``` javascript
+// let myArray = ['Marvin','Ferris','Brynn','Damian','Whitney','Leila','Frances','Keith','Cara','Ainsley','Adrian','Adam','Iona','Akeem','Scarlett','Murphy','Angelica','Georgia','Calvin','Amy','Merrill','Gloria','Charissa','Colorado','Inga','Eden','Carla','Hedley','Thaddeus','Maggie','Kimberly','Ezra','Kane','Candace','Cade','Dante','Scott','Philip','Dieter','Kristen','Nerea','Ivor','Stacey','Tanner','Judith','Emery','Lionel','Josiah','Wesley','Anne','Lane','Kameko','Colleen','Travis','Abdul','Scarlet','Quail','Larissa','Palmer','Irene','Cherokee','Rajah','Maxine','Yen','Bruce','Matthew','Ivan','Georgia','Charissa','Thaddeus','Jack','Dean','Florence','Madonna','Dennis','Zeus','Destiny','Maia','Mara','Florence','Anika','Brenda','Jocelyn','Zia','Kiona','Lars','Molly','Gregory','Felix','William','Dahlia','Gil','Byron','Daria','Nevada','Claudia','Zelenia','Nathan','Judah','Sheila']
+
+
+class FacebookUser
+{
+
+    constructor(name, age)
+    {
+        this.name_p= name;
+        this.age_p= age;
+    }
+}
+
+
+function main () {
+    let myArray = ['Marvin', 'Ferris', 'Brynn', 'Damian', 'Whitney', 'Leila', 'Frances', 'Keith', 'Cara', 'Ainsley', 'Adrian', 'Adam', 'Iona', 'Akeem', 'Scarlett', 'Murphy', 'Angelica', 'Georgia', 'Calvin', 'Amy', 'Merrill', 'Gloria', 'Charissa', 'Colorado', 'Inga', 'Eden', 'Carla', 'Hedley', 'Thaddeus', 'Maggie', 'Kimberly', 'Ezra', 'Kane', 'Candace', 'Cade', 'Dante', 'Scott', 'Philip', 'Dieter', 'Kristen', 'Nerea', 'Ivor', 'Stacey', 'Tanner', 'Judith', 'Emery', 'Lionel', 'Josiah', 'Wesley', 'Anne', 'Lane', 'Kameko', 'Colleen', 'Travis', 'Abdul', 'Scarlet', 'Quail', 'Larissa', 'Palmer', 'Irene', 'Cherokee', 'Rajah', 'Maxine', 'Yen', 'Bruce', 'Matthew', 'Ivan', 'Georgia', 'Charissa', 'Thaddeus', 'Jack', 'Dean', 'Florence', 'Madonna', 'Dennis', 'Zeus', 'Destiny', 'Maia', 'Mara', 'Florence', 'Anika', 'Brenda', 'Jocelyn', 'Zia', 'Kiona', 'Lars', 'Molly', 'Gregory', 'Felix', 'William', 'Dahlia', 'Gil', 'Byron', 'Daria', 'Nevada', 'Claudia', 'Zelenia', 'Nathan', 'Judah', 'Sheila']
+    let myAge = [28, 26, 27, 44, 65, 22, 89, 109];
+
+
+    let array_of_instantces = [];
+    myArray.forEach
+    (
+        function (element) {
+            let newName = new FacebookUser(element);
+            //console.log(newName);
+
+            array_of_instantces.push(newName);
+            //console.log (array_of_instantces);
+
+        }
+    );
+
+    console.log(array_of_instantces);
+
+    let newstring = [];
+
+    array_of_instantces.forEach(
+        function (element) {
+
+            newstring = newstring + `
+<h1 style='color:#ffff3a;background:red;text-shadow: -2px 2px 2px #000000;'>${element.name_p}</h1>
+`;
+        });
+
+console.log(newstring);
+
+
+    document.getElementById("classwork").innerHTML = newstring;
+}
+
+
+
+main();
+
+
+
+
+
+
+//
+// ### Challenge
+// In your HTML file, include in below div tag in the position as seen below:
+//     ```
+// <body>
+// <div id="classwork"></div>
+// <script src="index.js"></script>
+// </body>
+// ```
+// Using the array you created in the problem above,
+// concatenate every element's name attribute to the [OBJECT NAME] position below. Make sure each
+// element is no longer an array and concatenating each new string to a new variable.
+//
+//     ```
+// <h1 style='color:blue;background:red;text-shadow: -2px 2px 2px #000000;'>[OBJECT NAME]</h1>
+// ```
+// Lastly, assign the new string variable to the code below:
+//
+//     ```
+// document.getElementById("classwork").innerHTML = [NEW STRING]
+// ```
